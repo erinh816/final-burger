d2d63f99021c3bcb6f0f3a3c0567f74865c1bb5d
erinh816 <erinh816@gmail.com>
2018-09-22 22:51:50 -0400

    add

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Angus Burger');
+INSERT INTO burgers (name, eat) VALUES ('Cheese Bacon Burger', true);
+INSERT INTO burgers (name, eat) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, eat) VALUES ('Cheese Burger', true);
+
